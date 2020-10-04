dd99656afc0730a0bf3684d7bdf8578023729eff
JusufAmg <jamgad23@gmail.com>
2020-10-04 05:48:12 +0300

    third

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -248,7 +248,7 @@ string LinuxParser::User(int pid) {
   string uid = LinuxParser::Uid( pid);
   std::ifstream stream(kPasswordPath);
   if (stream) {
-    while(std:::getline(stream, line)){
+    while(std::getline(stream, line)){
         std::replace(line.begin(), line.end(), ':', ' ');
         std::istringstream linestream(line);
         while(linestream >> user >> buffer >> key){
